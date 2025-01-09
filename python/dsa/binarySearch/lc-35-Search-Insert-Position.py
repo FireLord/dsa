f71a3c429913cee09d2935ebb75101442cef3fd4
FireLord <firelord.xda@gmail.com>
2025-01-09 23:26:35 +0530

    add lower and upper bound functions for binary search

---
+++ python/dsa/binarySearch/lc-35-Search-Insert-Position.py
@@ -0,0 +1,24 @@
+def lowerBound(arr: [int], x: int) -> int:
+    n = len(arr)
+    low = 0
+    high = n - 1
+    ans = n
+
+    while low <= high:
+        mid = (low + high) // 2
+        # maybe an answer
+        if arr[mid] >= x:
+            ans = mid
+            # look for smaller index on the left
+            high = mid - 1
+        else:
+            low = mid + 1  # look on the right
+
+    return ans
+
+def main():
+    k = int(input())
+    nums = [int(x) for x in input().split(",")]
+    print(lowerBound(nums, k))
+
+main()
